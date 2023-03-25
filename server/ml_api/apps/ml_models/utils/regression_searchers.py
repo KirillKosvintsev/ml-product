495d9a79f9e58f36af06ab5ada76b7579192fad0
bountyHntr <bountyhntr1337@gmail.com>
2023-03-25 14:12:12 +0300

    added bagging regressor

--- server/ml_api/apps/ml_models/utils/regression_searchers.py
+++ server/ml_api/apps/ml_models/utils/regression_searchers.py
@@ -6,4 +6,7 @@ REGRESSION_CONFIG = {
         'criterion': hp.choice(label='criterion', options=['gini', 'entropy', 'squared_error', 'friedman_mse', 'absolute_error', 'poisson']),
         'max_depth': hp.randint(label='max_depth', low=3, high=30),
     },
+    'BaggingRegressor': {
+        'n_estimators': hp.randint(label="n_estimators", low=10, high=100),
+    }
 }
