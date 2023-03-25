73bbdd96aacb5a82ae4cbdf7c130cbedc0a4fea0
bountyHntr <bountyhntr1337@gmail.com>
2023-03-25 14:56:16 +0300

    added LinearRegression and SGDRegressor

--- server/ml_api/apps/ml_models/utils/regression_searchers.py
+++ server/ml_api/apps/ml_models/utils/regression_searchers.py
@@ -25,5 +25,16 @@ REGRESSION_CONFIG = {
         'n_estimators': hp.randint(label='n_estimators', low=50, high=300),
         'criterion': hp.choice(label='criterion', options=['squared_error', 'absolute_error', 'friedman_mse', 'poisson']),
         'max_depth': hp.randint(label='max_depth', low=3, high=20),
-    }
+    },
+    'LinearRegression': {
+    },
+    'SGDRegressor': {
+       'loss': hp.choice(label='loss', options=['squared_error', 'huber', 'epsilon_insensitive', 'squared_epsilon_insensitive']),
+       'penalty': hp.choice(label='penalty', options=['l2', 'l1', 'elasticnet', 'none']),
+       'alpha': hp.uniform('alpha', 1e-6, 1e-3),
+       'l1_ratio': hp.uniform('l1_ratio', 0.05, 0.95),
+       'learning_rate': hp.choice(label='learning_rate', options=['constant', 'optimal', 'invscaling', 'adaptive']),
+       'eta0': hp.uniform('eta0', 1e-4, 1e-1),
+       'power_t': hp.uniform('power_t', 0.1, 0.9),
+    },
 }
